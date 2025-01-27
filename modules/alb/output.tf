a898645f387cd7c775799308d31994b4600603b9
sibanando <sibhanayak@hotmail.com>
2025-01-27 09:44:51 +0530

    sftp-infra-module-created

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -1,11 +1,8 @@
-output "alb_dns_name" {
-  description = "The DNS name of the Application Load Balancer"
-  value       = aws_lb.internal.dns_name
-}
+# Duplicate outputs removed to avoid conflicts
 
-output "alb_zone_id" {
-  description = "The zone ID of the Application Load Balancer"
-  value       = aws_lb.internal.zone_id
+output "target_group_arn" {
+  description = "The ARN of the target group for SFTP"
+  value       = aws_lb_target_group.sftp_tg.arn
 }
 
 output "target_group_arn" {
